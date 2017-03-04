cc8161f959f9b7e3900fd31f806a40c17ef3c9ae
Miyaura <cafe.kobe@gmail.com>
2017-03-04 09:23:59 +0900

    Sample Code Upload.

---
+++ Assets/UWPBridgeSamples/Scripts/SetSampleText.cs
@@ -0,0 +1,37 @@
+﻿// Copyright(c) 2017 Takahiro Miyaura
+
+using UnityEngine;
+
+public class SetSampleText : MonoBehaviour
+{
+    public ISetSampleTextService service;
+
+    public GameObject Text3D;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        // Initialize Service From UWP.
+        service = UWPBridgeService.GetService<ISetSampleTextService>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //If Service is not Initialize,Call 'Start()'.
+        if (service == null)
+            Start();
+
+        //Call Service.
+        var textMesh = Text3D.GetComponent<TextMesh>();
+        textMesh.text = service.GetText();
+    }
+
+    //ISetSampleTextService is interface for class create on UWP Project. 
+    public interface ISetSampleTextService : UWPBridgeService.IUWPBridgeService
+    {
+        // Get string for 3DText.
+        string GetText();
+    }
+}
---
+++ UWP/UWPBridgeSamples/SetSampleTextService.cs
@@ -0,0 +1,12 @@
+﻿// Copyright(c) 2017 Takahiro Miyaura
+
+namespace UWPBridgeSamples
+{
+    public class SetSampleTextService : SetSampleText.ISetSampleTextService
+    {
+        public string GetText()
+        {
+            return "Sample Text For UWP";
+        }
+    }
+}
